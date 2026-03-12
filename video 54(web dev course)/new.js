f4e150b2c5c756bcb2eef8e5c4cc1c5b4ecccc3b
HarshadPatilDevCeo <harshadcreative3@gmail.com>
2026-03-12 12:30:14 +0530

    "day 54"

---
+++ video 54(web dev course)/new.js
@@ -0,0 +1,3 @@
+console.log("code is running......")
+console.log("code is running fast..")
+console.log("code id running fast and securly...")
---
+++ video 54(web dev course)/script.js
@@ -0,0 +1,19 @@
+console.log("code is running......")
+console.log("code is running fast..")
+console.log("code id running fast and securly.....")
+console.log("your number is " +a)
+
+
+var istrue= confirm("Are your sure you want to leave this page and burn your comment")
+
+if(istrue){
+    console.log("computer is blasting")
+}
+
+else{
+    console.log("computer is not blasting")
+}
+
+document.title="Hey I am Good"
+
+document.body.style.backgroundColor = "red"
