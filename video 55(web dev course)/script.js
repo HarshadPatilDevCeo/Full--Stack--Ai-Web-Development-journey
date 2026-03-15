a5b11aa0838eaade91b3f31ec819d1a4618a9d85
HarshadPatilDevCeo <harshadcreative3@gmail.com>
2026-03-15 11:13:41 +0530

    "day 55"

---
+++ video 55(web dev course)/script.js
@@ -0,0 +1,13 @@
+
+//console.log(o)
+//o.salary = "10lakhs";
+//console.log(o);
+//o.salary = "20lakhs";
+//console.log(o);
+ 
+
+let o = {
+    "name": "harshad",
+    "jobcode":4101
+}
+console.log(o)
