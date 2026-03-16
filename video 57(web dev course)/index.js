4394c9285081ae768c3acf4e281a64d7e382b53a
HarshadPatilDevCeo <harshadcreative3@gmail.com>
2026-03-16 13:53:08 +0530

    "day 57"

---
+++ video 57(web dev course)/index.js
@@ -0,0 +1,22 @@
+//console.log("tutorial of loops");
+
+//let a = 1;
+//console.log(a+b);
+
+//let obj = {
+  //  name:'harshad',
+    //role:'web dev',
+    //eakhub:'peakhub',
+
+
+//for(const key in obj){
+
+
+let i=0;
+do{
+    console.log(i);
+}
+
+  
+    
+
