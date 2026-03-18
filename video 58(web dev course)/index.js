7c138cdfbffaf537f1324bae7bb7f4550f7dc6f3
HarshadPatilDevCeo <harshadcreative3@gmail.com>
2026-03-18 13:24:19 +0530

    "day 58"

---
+++ video 58(web dev course)/index.js
@@ -0,0 +1,20 @@
+//console.log("i am harshad")
+//console.log("hey i am harshad and i am smart")
+//console.log("hey i am harshad i am good at skill")
+
+
+//function  name (params){
+//console.log(("hey" + name + "harshad" ));
+//}
+
+//nice("harshad");
+
+
+function sum (a,b,c){
+    console.log(a+b);
+}
+sum(3,5,5)
+
+
+
+
