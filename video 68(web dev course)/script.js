7e7a87680c70333ef593a0eac0ecb44df55a1c22
HarshadPatilDevCeo <harshadcreative3@gmail.com>
2026-04-01 11:28:54 +0530

    "day 68"

---
+++ video 68(web dev course)/script.js
@@ -0,0 +1,12 @@
+//console.log("harshads is best ")
+//let boxes = document.getElementsByClassName("box")
+//console.log("boxes")
+
+//boxes(2) .style.bakgroundColor = "red"
+
+//document.getElementById("red").style.backgroundColor = "red"
+
+document.querySelector(".box").foreach(e=>{e.style.backgroundColor = "red"})
+
+
+
