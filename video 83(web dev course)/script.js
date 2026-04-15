6f16c78341c5370f358433d6baa4092af509ef91
HarshadPatilDevCeo <harshadcreative3@gmail.com>
2026-04-15 14:44:19 +0530

    "day 83"

---
+++ video 83(web dev course)/script.js
@@ -0,0 +1,18 @@
+let student = ["harshad","arpita" ,"sharvani" , "vedika", "swarali", "shivendra", "shivanya"]
+
+for ( const student of students ){
+    if (student.lenght < 6 ){
+        houses.push("harshu")
+
+    }
+    else if ( student.lenght < 8){
+        houses.push ("harshad")
+    }
+
+    else if (student.lenght < 12){
+        house.push("harshad patil")
+    }
+    
+}
+
+console.log(houses)
